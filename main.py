6c13b4e0ab1d7d5a7c2bc82c2b7f3916fb428d4c
Igor Tkachuk <I.Tka4yk@gmail.com>
2023-01-09 21:37:30 +0200

    feat: add uniqueness of showed images, store sheduled jobs in Redis, modified Docer-compse file

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from unsplash.api import Api
 from unsplash.auth import Auth
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.jobstores.redis import RedisJobStore
+from apscheduler_di import ContextSchedulerDecorator
+
+from redis import Redis
 
 from config import load_config
 
@@ -23,11 +27,21 @@ auth = Auth(
 api = Api(auth)
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+r = Redis(host="redis")
 
 bot = Bot(token=config.tgbot.token)
 dp = Dispatcher(bot)
 
-scheduler = AsyncIOScheduler()
+job_stores = {
+    "default": RedisJobStore(
+        jobs_key="dispatched_trips_jobs", run_times_key="dispatched_trips_running",
+        host="redis", port=6379
+    )
+}
+
+scheduler = ContextSchedulerDecorator(AsyncIOScheduler(jobstores=job_stores))
 
 chats: List[str] = []
 
@@ -46,8 +60,18 @@ async def send_photo(chat_id: int):
 
 @dp.message_handler(commands=['katze'])
 async def send_katze(message: types.Message):
-    print(f"---> {message.chat.id}")
-    photo = api.photo.random(query="cat")
+    chat_id = message.chat.id
+
+    while True:
+        photo = api.photo.random(query="cat")
+        photo_id = photo[0].id
+
+        if not r.sismember(str(chat_id) + "_showed_katzes", photo_id):
+            break
+
+        logger.info(f"Image {photo_id} is showed earlier. Skipped!")
+
+    r.sadd(str(chat_id) + "_showed_katzes", photo_id)
     photo_url = photo[0].urls.small
 
     print(photo_url)
@@ -65,6 +89,27 @@ async def plan_scheduler(message: types.Message):
     await message.answer("Scheduled at 10:00, 14:00, 18:00, 22:00")
 
 
+@dp.message_handler(commands=['configuration'])
+async def get_configuration(message: types.Message):
+    result = "Photo distribution planed at:\n"
+    jobs_count = 0
+
+    jobs = scheduler.get_jobs()
+    for job in jobs:
+        if job.args[0] == message.chat.id:
+            jobs_count += 1
+            next_run_time = job.next_run_time.strftime("%d.%m.%Y %H:%M")
+            result += next_run_time + "\n"
+
+    if jobs_count == 0:
+        await message.answer("No distribution planed")
+    else:
+        await message.answer(result)
+
+
 if __name__ == '__main__':
-    scheduler.start()
-    executor.start_polling(dp, on_startup=init, skip_updates=True)
+    try:
+        scheduler.start()
+        executor.start_polling(dp, on_startup=init, skip_updates=True)
+    except (KeyboardInterrupt, SystemExit):
+        logger.error("Bot stopped!")
